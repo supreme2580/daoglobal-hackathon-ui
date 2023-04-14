15ea510293a7ec8091c26df70f6126f28e80a4ad
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-14 20:56:26 +0100

    working on the cards

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,34 @@
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
+  daisyui: {
+    themes: [
+      {
+        darkmode: {
+          primary: "#e24147",
+          secondary: "#5bd891",
+          accent: "#978cd8",
+          neutral: "#1A192E",
+          "base-100": "#E6E4F1",
+          info: "#1986EB",
+          success: "#119C65",
+          warning: "#F4D371",
+          error: "#F15232",
+        },
+        lightmode: {
+            primary: "#e24147",
+            secondary: "#5bd891",
+            accent: "#978cd8",
+            neutral: "#1A192E",
+            "base-100": "#E6E4F1",
+            info: "#1986EB",
+            success: "#119C65",
+            warning: "#F4D371",
+            error: "#F15232",
+          },
+      },
+    ],
+  },
   /**
    * If you have the "experimental: { appDir: true }" setting enabled, then you
    * must comment the below `i18n` config out.
--- src/components/MemberCard.tsx
+++ src/components/MemberCard.tsx
@@ -1,11 +1,11 @@
-import { IdentificationIcon } from "@heroicons/react/24/outline"
+import { ArrowRightIcon, IdentificationIcon } from "@heroicons/react/24/outline"
 import Image from "next/legacy/image"
 import truncateEthAddress from 'truncate-eth-address'
 
 export default function MemberCard() {
     return(
         <div className="card w-full bg-black text-neutral-content">
-            <div className="card-body items-center text-center">
+            <div className="card-body items-center text-center space-y-1">
                 <div className="w-full flex justify-between items-center">
                     <div className="flex items-center space-x-3 w-full">
                         <div className="w-10 h-10 relative shrink-0">
@@ -17,10 +17,23 @@ export default function MemberCard() {
                         <IdentificationIcon className="w-6 h-6 text-white" />
                     </div>
                 </div>
-                <p>We are using cookies for no reason.</p>
-                <div className="card-actions justify-end">
-                <button className="btn btn-primary">Accept</button>
-                <button className="btn btn-ghost">Deny</button>
+                <div className="w-full space-y-1">
+                    <div className="flex space-x-2 items-center justify-start w-full">
+                        <div className="w-4 h-4 relative">
+                            <Image src={"/bal-light.png"} layout="fill" alt="image" />
+                        </div>
+                        <p className="text-start">Token balance: 0.07 ETH</p>
+                    </div>
+                    <div className="flex space-x-2 items-center justify-start w-full">
+                        <div className="w-4 h-4 relative">
+                            <Image src={"/voting-power-light.png"} layout="fill" alt="image" />
+                        </div>
+                        <p className="text-start">voting power: 2.07</p>
+                    </div>
+                    <div className="flex items-center justify-start w-40">
+                        <p className="text-sm text-start w-10">View member profile</p>
+                        <ArrowRightIcon className="w-4 h-4 text-daoboxg" />
+                    </div>
                 </div>
             </div>
         </div>
