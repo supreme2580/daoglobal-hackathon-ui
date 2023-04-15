dff3a104bd59c0772e42724e7f89d5835e5b0a3b
pythonpete32 <aaron@blackscale.io>
2023-04-16 02:55:26 +0400

    be quiet pedantic linter

--- src/pages/voting.tsx
+++ src/pages/voting.tsx
@@ -1,10 +1,16 @@
 import { daoAddressOrEns } from "@constants/daoConfig";
-import {
-  TokenVotingProposalListItem,
-  useFetchProposals,
-} from "@daobox/use-aragon";
+import { useFetchProposals } from "@daobox/use-aragon";
 import { type NextPage } from "next";
 import Head from "next/head";
+type ReplacerFunction = (key: string, value: unknown) => unknown;
+
+const bigintToStringReplacer: ReplacerFunction = (key, value) => {
+  if (typeof value === "bigint") {
+    return value.toString();
+  } else {
+    return value;
+  }
+};
 
 const Voting: NextPage = () => {
   const { data } = useFetchProposals({ daoAddressOrEns });
@@ -16,13 +22,9 @@ const Voting: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>DAOGlobal Voting UI</h1>
-      {data?.map((proposal: TokenVotingProposalListItem, index: React.Key) => (
+      {data?.map((proposal, index: React.Key) => (
         <pre key={index}>
-          {JSON.stringify(
-            proposal,
-            (_, v) => (typeof v === "bigint" ? v.toString() : v),
-            2
-          )}
+          {JSON.stringify(proposal, bigintToStringReplacer, 2)}
         </pre>
       ))}
     </>
