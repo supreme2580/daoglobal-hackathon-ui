f4d6488b5330295f922ea427e19af7048ce58d10
pythonpete32 <aaron@blackscale.io>
2023-04-29 16:46:15 +0400

    Member card use live data and blockie

--- src/components/members/MembersCardCount.tsx
+++ src/components/members/MembersCardCount.tsx
@@ -1,22 +1,22 @@
 import Button from "./Button";
 
-export default function MembersCardCount() {
-    return (
-      <div className="card w-full bg-[#191B1E] text-neutral-content">
-        <div className="flex h-full w-full flex-col space-y-2.5 px-8 py-8 sm:flex-row sm:items-center sm:justify-between sm:space-y-0">
-          <div className="space-y-2.5">
-            <h2 className="card-title text-white">10 Members</h2>
-            <p className="font-semibold text-white">Wallet based</p>
-          </div>
-          <div>
-            <Button
-              text="Manage Members"
-              clickFunction={() => {
-                console.log("Clicked");
-              }}
-            />
-          </div>
+export default function MembersCardCount({ memberCount }: { memberCount: number | undefined }) {
+  return (
+    <div className="card w-full bg-[#191B1E] text-neutral-content">
+      <div className="flex h-full w-full flex-col space-y-2.5 px-8 py-8 sm:flex-row sm:items-center sm:justify-between sm:space-y-0">
+        <div className="space-y-2.5">
+          <h2 className="card-title text-white">{memberCount ?? 0} Members</h2>
+          <p className="font-semibold text-white">Wallet based</p>
+        </div>
+        <div>
+          <Button
+            text="Manage Members"
+            clickFunction={() => {
+              console.log("Clicked");
+            }}
+          />
         </div>
       </div>
-    );
+    </div>
+  );
 }
