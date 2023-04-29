9f2f6696a4338170f6d59652138f0f101ca94632
pythonpete32 <aaron@blackscale.io>
2023-04-29 12:53:58 +0400

    add DAO Box NFT to consts

--- src/constants/daoConfig.ts
+++ src/constants/daoConfig.ts
@@ -1,3 +1,10 @@
-export const daoAddressOrEns = "0x6f07aa7af27e0e06a08a1a17e04c4b0eb11300ab";
-export const votingPluginAddress = "0x8eaf189dbe3524667d25684645aba1c71c02d8db";
-export const daoTokenAddress = "0xdd6067e741e79ea707d5185f72e52bc6a9784fd1";
+import { BN } from "@lib/numbers";
+import { Address } from "wagmi";
+
+export const daoAddressOrEns: Address = "0x6f07aa7af27e0e06a08a1a17e04c4b0eb11300ab";
+export const votingPluginAddress: Address = "0x8eaf189dbe3524667d25684645aba1c71c02d8db";
+export const daoTokenAddress: Address = "0xdd6067e741e79ea707d5185f72e52bc6a9784fd1";
+export const DaoBoxFollowNft: Address = "0x03bD27B6FE4f0B3F2C2C9a4289cA2bfcE1DDE95c";
+export const LensHub: Address = "0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d";
+
+export const DaoBoxProfileId = BN(115969);
