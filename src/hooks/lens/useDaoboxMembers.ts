4cff8e227ec0340e0575ce60e46cad471a3ca162
pythonpete32 <aaron@blackscale.io>
2023-04-29 16:46:33 +0400

    hook to fetch members from api

--- src/hooks/lens/useDaoboxMembers.ts
+++ src/hooks/lens/useDaoboxMembers.ts
@@ -1,11 +1,13 @@
-import { useQuery } from "wagmi";
+import { Address, useQuery } from "wagmi";
 
 const fetchDaoboxMembers = async () => {
   const response = await fetch("/api/daobox-members");
   if (!response.ok) throw new Error("Failed to fetch data from daobox-members API");
-  return await response.json();
+  const result = (await response.json()).data.owners;
+
+  return result;
 };
 
 export const useDaoboxMembers = () => {
-  return useQuery(["daobox-members"], fetchDaoboxMembers);
+  return useQuery<Address[]>(["daobox-members"], fetchDaoboxMembers);
 };
