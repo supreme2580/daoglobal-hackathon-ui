0c2b6edb9d16bb2dd2f98b27dc4608f33f05fda5
pythonpete32 <aaron@blackscale.io>
2023-05-07 15:05:22 +0400

    test hooks

--- src/pages/ops/index.tsx
+++ src/pages/ops/index.tsx
@@ -1,5 +1,7 @@
 import { BN, useOpProposal, useOpProposals } from "@hooks/op/read";
-import { useNewOpProposal } from "@hooks/op/write";
+import { useChallengeProposal, useNewOpProposal } from "@hooks/op/write";
+import { useCancelProposal } from "@hooks/op/write/useCancelProposal";
+import { useExecuteProposal } from "@hooks/op/write/useExecuteProposal";
 
 import { type NextPage } from "next";
 import Head from "next/head";
@@ -47,8 +49,26 @@ const Proposals: NextPage = () => {
 export default Proposals;
 
 function ProposalCard({ proposal }: any) {
+  const { write: cancelProposal } = useCancelProposal(proposal.proposalId);
+  const { write: challangeProposal } = useChallengeProposal(proposal.proposalId);
+
+  const { write: executeProposal } = useExecuteProposal(proposal.proposalId);
+  const handleExecute = () => {
+    console.log(`execute ID: ${proposal.proposalId}`);
+    executeProposal?.();
+  };
+  const handleCancel = () => {
+    console.log(`cancel ID: ${proposal.proposalId}`);
+    cancelProposal?.();
+  };
+
+  const handleChallange = () => {
+    console.log(`challange ID: ${proposal.proposalId}`);
+    challangeProposal?.();
+  };
+
   return (
-    <div className="card mb-2 p-2">
+    <div className="card mb-2 bg-base-300 p-6 shadow-xl">
       <h5 className="card-title">Proposal ID: {proposal.proposalId}</h5>
       <p>Dispute Status: {proposal.disputeStatus}</p>
       <p>Status: {proposal.status}</p>
@@ -57,7 +77,17 @@ function ProposalCard({ proposal }: any) {
       <p>Dispute ID: {proposal.disputeId?.toString()}</p>
       <p>Proposer: {proposal?.proposer}</p>
       {proposal?.metadata && <p>{JSON.stringify(proposal.metadata, null, 2)}</p>}
-      {/* <p>Metadata: {proposal?.metadata}</p> */}
+      <div className="flex w-full justify-center space-x-4 p-2">
+        <button className="btn-primary btn" onClick={() => handleExecute()}>
+          execute
+        </button>
+        <button className="btn-primary btn" onClick={() => handleCancel()}>
+          cancel
+        </button>
+        <button className="btn-primary btn" onClick={() => handleChallange()}>
+          challange
+        </button>
+      </div>
     </div>
   );
 }
